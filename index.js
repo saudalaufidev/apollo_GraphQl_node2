44bbe533444fdda78f6ac3c3d457d157efbe5f61
saudalaufidev <62303759+saudalaufidev@users.noreply.github.com>
2021-07-14 14:03:21 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const Mutation=require('./resolvers/Mutation')
 const User=require('./resolvers/User')
 const Post=require('./resolvers/Post')
 
+app.get('/',(req,res,next)=>{
+    res.redirect('/graphql')
+    })
+    app.get('/graphql',(req,res,next)=>{
+        res.send("Send a POST request to query this endpoint -- Saud")
+    })
+    
 const server=new ApolloServer({
     typeDefs,
     resolvers:{
@@ -22,10 +29,5 @@ const server=new ApolloServer({
 })
 server.applyMiddleware({app})
 
-app.get('/',(req,res,next)=>{
-res.redirect('/graphql')
-})
-app.get('/graphql',(req,res,next)=>{
-    res.send("Send a POST request to query this endpoint -- Saud")
-})
+
 app.listen({ port: process.env.PORT || 4000 },()=>console.log("server working on http://localhost:4000"+server.graphqlPath))
